d2fbb881628cbe07b49e531a6e2aa7601c2ca37c
mshi-uiuc <mshi16@illinois.edu>
2018-11-27 14:51:13 -0600

    Added several images

--- finalproject/src/scenes/scene_story.cpp
+++ finalproject/src/scenes/scene_story.cpp
@@ -3,6 +3,8 @@
 namespace finalproject {
 	Scene_Story::Scene_Story(const json &story_file) {
 		file = story_file;
+		data = file["story"].at(current_index);
+
 		for (auto& item : file["inventory"]) {
 			inventory.push_back(InventoryItem(item["name"], item["desc"]));
 		}
@@ -16,34 +18,62 @@ namespace finalproject {
 
 	void Scene_Story::update() {
 		if (shouldUpdate) {
-			json data = file["story"].at(current_index);
-			
-			text_bg.clear();
-
-			if (data.contains("bg")) {
-				bg.load(data["bg"].get<std::string>());
-			}
-			if (data.contains("bgm")) {
-
-			}
-			if (data.contains("text")) {
-				text_bg.load("text_bg.png");
-			}
-
+			updateImages();
+			updateTextbox();
+			updateSounds();
 			shouldUpdate = false;
+		}
+	}
+
+	void Scene_Story::updateImages() {
+		if (data.contains("bg")) {
+			bg.load(data["bg"].get<std::string>());
+		}
+		if (data.contains("image")) {
+			if (data["image"].size() > 0) {
+				sprite.load(data["image"].get<std::string>());
+			} else {
+				sprite.clear();
+			}
+		}
+		if (data.contains("overlay")) {
+			if (data["overlay"].size() > 0) {
+				overlay.load(data["overlay"].get<std::string>());
+			} else {
+				overlay.clear();
+			}
+		}
+	}
+
+	void Scene_Story::updateTextbox() {
+		if (data.contains("text")) {
+			std::cout << data["text"] << std::endl;
+			text_bg.load("text_bg.png");
+		} else {
+			text_bg.clear();
+		}
+	}
+
+	void Scene_Story::updateSounds() {
+		if (data.contains("bgm")) {
+			bgm_channel.load(data["bgm"].get<std::string>());
+			bgm_channel.setLoop(true);
+			bgm_channel.play();
 		}
 	}
 
 	void Scene_Story::draw() {
 		ofSetColor(255);
 		bg.draw(0, 0);
+		sprite.draw(0, 0);
+		overlay.draw(0, 0);
 		text_bg.draw(0, 0);
 	}
 
 	void Scene_Story::processKey(int key) {
 		try {
 			current_index++;
-			file["story"].at(current_index); //check that access is available
+			data = file["story"].at(current_index); //check that access is available
 			shouldUpdate = true;
 
 		} catch (std::out_of_range) {
--- finalproject/src/scenes/scene_story.h
+++ finalproject/src/scenes/scene_story.h
@@ -9,14 +9,24 @@ namespace finalproject {
 
 	class Scene_Story : public Scene {
 		json file;
+		json data;
+		int current_index = 0;
+
 		std::vector<InventoryItem> inventory;
 
-		int current_index = 0;
 		bool shouldUpdate = true;
 
+		ofImage bg;
+		ofImage sprite;
+		ofImage overlay;
 		ofImage text_bg;
 
+		ofSoundPlayer bgm_channel;
+
 		void loadResources();
+		void updateImages();
+		void updateTextbox();
+		void updateSounds();
 
 	public:
 		Scene_Story(const json &story_file);
