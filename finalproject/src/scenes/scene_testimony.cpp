88e58bc2a64cbea085a5296fc18e086e108e2933
mshi-uiuc <mshi16@illinois.edu>
2018-12-02 22:01:53 -0600

    Started testimony scene

---
+++ finalproject/src/scenes/scene_testimony.cpp
@@ -0,0 +1,2 @@
+#include "scene_testimony.h"
+
---
+++ finalproject/src/scenes/scene_testimony.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "base.h"
+#include "scenemanager.h"
+#include "../libs/json.hpp"
+
+using nlohmann::json;
+
+namespace finalproject {
+
+	class Scene_Testimony : public Scene {
+
+	};
+
+} //namespace finalproject
